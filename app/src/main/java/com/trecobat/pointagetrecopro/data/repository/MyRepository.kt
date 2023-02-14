f71efa566f7481441553ed2ddaed6badeefe940f
Vincent BOULARD <vincentbo@trecoland.fr>
2023-02-14 15:53:39 +0100

    DataAccessStrategy remove

--- app/src/main/java/com/trecobat/pointagetrecopro/data/repository/MyRepository.kt
+++ app/src/main/java/com/trecobat/pointagetrecopro/data/repository/MyRepository.kt
@@ -1,11 +1,15 @@
 package com.trecobat.pointagetrecopro.data.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
 import com.trecobat.pointagetrecopro.data.entities.PendingRequest
 import com.trecobat.pointagetrecopro.data.entities.Pointage
 import com.trecobat.pointagetrecopro.data.local.MyDao
 import com.trecobat.pointagetrecopro.data.remote.BaseDataSource
-import com.trecobat.pointagetrecopro.utils.performGetOperation
-import com.trecobat.pointagetrecopro.utils.performPostOperation
+import com.trecobat.pointagetrecopro.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import timber.log.Timber
 
 class MyRepository (
     private val remoteDataSource: BaseDataSource,
@@ -53,4 +57,47 @@ class MyRepository (
         networkCall = { remoteDataSource.getFilesOfTache(id) },
         saveCallResult = { localDataSource.insertAllFiles(it) }
     )
+
+
+
+    private fun <T, A> performGetOperation(
+        databaseQuery: () -> LiveData<T>,
+        networkCall: suspend () -> Resource<A>,
+        saveCallResult: suspend (A) -> Unit
+    ): LiveData<Resource<T>> =
+        liveData(Dispatchers.IO) {
+            emit(Resource.loading())
+            val source = databaseQuery.invoke().map { Resource.success(it) }
+            emitSource(source)
+
+            val responseStatus = networkCall.invoke()
+            if (responseStatus.status == Resource.Status.SUCCESS) {
+                saveCallResult(responseStatus.data!!)
+
+            } else if (responseStatus.status == Resource.Status.ERROR) {
+                emit(Resource.error(responseStatus.message!!))
+                emitSource(source)
+            }
+        }
+
+    private fun <A> performPostOperation(
+        networkCall: suspend () -> Resource<A>,
+        saveCallResult: suspend (A) -> Unit
+    ): LiveData<Resource<Nothing?>> =
+        liveData(Dispatchers.IO) {
+            Timber.i("performPostOperation")
+            emit(Resource.loading())
+
+            val responseStatus = networkCall.invoke()
+            if (responseStatus.status == Resource.Status.SUCCESS) {
+                Timber.i("performPostOperation SUCCESS")
+                saveCallResult(responseStatus.data!!)
+                emit(Resource.success(null))
+            } else if (responseStatus.status == Resource.Status.ERROR) {
+                Timber.i("performPostOperation ERROR")
+                emit(Resource.error(responseStatus.message!!))
+            } else {
+                Timber.i("performPostOperation ni SUCCESS ni ERROR")
+            }
+        }
 }
